116a558150178e5518c7a1e1d710b87981b56e01
BANQUETA <banquetox@gmail.com>
2023-02-24 22:35:18 -0600

    2

---
+++ 2.py
@@ -0,0 +1,12 @@
+# Haga un programa que dado un número entero N, imprima todos los números desde N hasta 1.
+# Ejemplo: si N=4, el programa imprime lo siguiente:
+# Los números desde 4 hasta 1, son:
+# 4
+# 3
+# 2
+# 1
+
+N = 4
+while N > 0:
+    print(N)
+    N -= 1
